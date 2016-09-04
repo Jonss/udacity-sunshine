5e8236eb6874aa64b1eb824242ecbe882db4da55
Jonss <joaosantana.ti@gmail.com>
2016-09-03 23:09:08 -0300

    ShareActionProvider

--- app/src/main/java/com/github/jonss/sunshine/DetailActivity.java
+++ app/src/main/java/com/github/jonss/sunshine/DetailActivity.java
@@ -1,16 +1,21 @@
 package com.github.jonss.sunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.preference.PreferenceManager;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private String weatherStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,26 +23,56 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Intent intent = getIntent();
+        weatherStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+
+        TextView tempTextView = (TextView) findViewById(R.id.detail_text);
+        tempTextView.setText(weatherStr);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                break;
+            case R.id.action_map:
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+                String cep = preferences.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
 
-        if(id == R.id.action_settings){
-            startActivity(new Intent(this, SettingsActivity.class));
-            return true;
+                Uri parse = Uri.parse("geo:0,0?q=" + cep.substring(0, 5));
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(parse);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+                break;
+            case R.id.action_share:
+                ShareActionProvider provider = new ShareActionProvider(this);
+                provider.setShareIntent(createShareForecastIntent());
+
+                MenuItemCompat.setActionProvider(item, provider);
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private Intent createShareForecastIntent() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, weatherStr + " #Sunshine");
+        return intent;
     }
 
 }
